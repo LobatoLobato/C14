9b457843e6aea8e6dec6fcb799d3dfd0095a4aeb
lobatolobato <felipelobato.rj@gmail.com>
2025-09-03 00:32:44 -0300

    Add googletest dependency

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 ROOT_DIR=$(dirname $(dirname $(realpath "$0")))
 
-cmake -S "$ROOT_DIR" -B "$ROOT_DIR/build" -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
+cmake -S "$ROOT_DIR" -B "$ROOT_DIR/build" -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build "$ROOT_DIR/build" --target $1
